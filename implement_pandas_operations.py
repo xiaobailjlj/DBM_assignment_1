0c0e8cdbfeb967481a08926de1fbeac1d1fa51c8
路静 <lujing@lujings-MacBook-Pro.local>
2024-09-17 22:56:22 +0200

    pandas

---
+++ implement_pandas_operations.py
@@ -0,0 +1,69 @@
+import pandas as pd
+
+# sql
+query_06 = '''
+select
+	sum(l_extendedprice * l_discount) as revenue
+from
+	lineitem
+where
+	l_shipdate >= date '1994-01-01'
+	and l_shipdate < date '1994-01-01' + interval '1' year
+	and l_discount between .06 - 0.01 and .06 + 0.01
+	and l_quantity < 24;
+'''
+
+# Set pandas display options to show all columns
+pd.set_option('display.max_columns', None)  # Show all columns
+pd.set_option('display.max_colwidth', None)  # Show full content of each column
+
+column_names = [
+    "l_orderkey",
+    "l_partkey",
+    "l_suppkey",
+    "l_linenumber",
+    "l_quantity",
+    "l_extendedprice",
+    "l_discount",
+    "l_tax",
+    "l_returnflag",
+    "l_linestatus",
+    "l_shipdate",
+    "l_commitdate",
+    "l_receiptdate",
+    "l_shipinstruct",
+    "l_shipmode",
+    "l_comment",
+    "l_omit"
+]
+
+# load dataset
+# lineitem = pd.read_csv('./SF-1/data/lineitem_tmp.tbl', sep='|', names=column_names, header=None, parse_dates=['l_shipdate', 'l_commitdate', 'l_receiptdate'])
+# lineitem = pd.read_csv('./SF-1/data/lineitem.tbl', sep='|', names=column_names, header=None, parse_dates=['l_shipdate', 'l_commitdate', 'l_receiptdate'])
+lineitem = pd.read_csv('./SF-1/data/lineitem.tbl', sep='|', names=column_names, header=None)
+
+
+# print 10 lines if the data is loaded successfully
+print(lineitem.head(2))
+
+# shipdate, start date and end date
+lineitem['l_shipdate'] = pd.to_datetime(lineitem['l_shipdate'])
+start_date = pd.to_datetime('1994-01-01')
+end_date = start_date + pd.DateOffset(years=1)
+
+print(f"*** start_date: {start_date}, end_date:, {end_date}")
+
+# filter
+filtered_df = lineitem[
+    (lineitem['l_shipdate'] >= start_date) &  # Date greater than or equal to '1994-01-01'
+    (lineitem['l_shipdate'] < end_date) &     # Date less than '1995-01-01'
+    (lineitem['l_discount'].between(0.05, 0.07)) &  # Discount between 0.05 and 0.07
+    (lineitem['l_quantity'] < 24)             # Quantity less than 24
+]
+
+# print 10 lines if the data is filtered successfully
+print(filtered_df.head(2))
+
+# calculate revenue
+revenue = (filtered_df['l_extendedprice'] * filtered_df['l_discount']).sum()
+print(f"Revenue: {revenue}")
